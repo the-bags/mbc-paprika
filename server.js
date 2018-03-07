d972c1084808dabead5b96ad1a3e1f09636c97c2
raymond <Raymond Perminov>
2018-03-07 19:19:09 +0200

    Simple UI

---
+++ server.js
@@ -0,0 +1,136 @@
+
+var Web3 = require('web3');
+var restify = require('restify');
+
+ 
+const server = restify.createServer({
+  name: 'app',
+  version: '1.0.0'
+});
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+server.use(
+    function crossOrigin(req,res,next){
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      return next();
+    }
+  );
+
+// server.get('/state', function (req, res, next) {
+//     var syncing = web3.eth.syncing;
+//     console.log('syncing:', syncing)
+//     res.send({
+//         syncing : syncing
+//     });
+//     return next();
+//   });
+
+// server.get('/account', function (req, res, next) {
+//     var coinbase = web3.eth.coinbase;
+//     console.log('coinbase:', coinbase)
+//     res.send({
+//         coinbase : coinbase
+//     });
+//     return next();
+// });
+server.get('/:instanceUrl/state', function (req, res, next) {
+    var web3 = new Web3();
+    // http://localhost:8545
+    var url = "http://"+req.params.instanceUrl;
+    console.log(url);
+    web3.setProvider(new web3.providers.HttpProvider(url));
+    if(web3.isConnected()) {
+        var syncing = web3.eth.syncing;
+        console.log('syncing:', syncing,' req:', req.params.instanceUrl)
+        res.send({
+            syncing : syncing
+        });
+     } else {
+        console.log('Error');
+        res.send({
+            syncing : "Error"
+        });
+     }
+    return next();
+});
+
+
+server.get('/:instanceUrl/account', function (req, res, next) {
+    var web3 = new Web3();
+    // http://localhost:8545
+    var url = "http://"+req.params.instanceUrl;
+    console.log(url);
+    web3.setProvider(new web3.providers.HttpProvider(url));
+    if(web3.isConnected()) {
+        var coinbase = web3.eth.coinbase;
+        console.log('coinbase:', coinbase,' req:', req.params.instanceUrl)
+        res.send({
+            coinbase : coinbase
+        });
+     } else {
+        console.log('Error');
+        res.send({
+            coinbase : "Error"
+        });
+     }
+    return next();
+});
+
+
+server.get('/:instanceUrl/block_number', function (req, res, next) {
+    var web3 = new Web3();
+    // http://localhost:8545
+    var url = "http://"+req.params.instanceUrl;
+    console.log(url);
+    web3.setProvider(new web3.providers.HttpProvider(url));
+    if(web3.isConnected()) {
+        var blockNumber = web3.eth.blockNumber;
+        console.log('blockNumber:', blockNumber,' req:', req.params.instanceUrl)
+        res.send({
+            blockNumber : blockNumber
+        });
+     } else {
+        console.log('Error');
+        res.send({
+            blockNumber : "Error"
+        });
+     }
+    return next();
+});
+
+server.get('/echo/:name', function (req, res, next) {
+    console.log('echo:',req.params.name);
+    res.send(req.params);
+    return next();
+});
+
+///server.get('/:instanceUrl/')
+
+server.listen(8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+
+//var web3 = new Web3();
+
+//console.log(web3);
+
+// if (typeof web3 !== 'undefined') {
+//     web3 = new Web3(web3.currentProvider);
+//   } else {
+//     // set the provider you want from Web3.providers
+//     web3 = new Web3(new Web3.providers.HttpProvider(""));
+//     var coinbase = web3.eth.coinbase;
+//     console.log(coinbase);
+//   }
+
+//var Web3 = require('../index.js');
+// var accounts = web3.eth.accounts;
+
+  
+//   var balance = web3.eth.getBalance(coinbase);
+
+ 
--- web/js/app.js
+++ web/js/app.js
@@ -4,19 +4,38 @@ var app = new Vue({
   data(){
     return {
       urlNode: '',
+      coinbase: '',
+      syncing: '',
+      blockNumber: '',
+      time: 15,
     }
   },
   methods: {
     getData(){
-      $.get("", {} ,function(t){
-      console.log('DATA:',t);
-    },"json");
+      var _this = this;
+      $.get("http://localhost:8080/"+_this.urlNode+"/block_number" ,function(res){
+        _this.blockNumber = res.blockNumber;
+      },"json");
+      $.get("http://localhost:8080/"+_this.urlNode+"/state" ,function(res){
+        _this.syncing = res.syncing;
+      },"json");
+      $.get("http://localhost:8080/"+_this.urlNode+"/account" ,function(res){
+        _this.coinbase = res.coinbase;
+      },"json");
     },
     sendURL(){
       console.log('Send URL');
+      this.getData();
     }
   },
   created() {
-
+    var _this = this;
+    setInterval(function() {
+      _this.time--;
+      if (_this.time<0) {
+        _this.time = 15;
+        _this.getData();
+      };
+    }, 1000);
   },
 })
